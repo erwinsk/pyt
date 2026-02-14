7c7f40b2ddf6c5e2c3df6822b487a8d9dedfea84
Erwin Setya <95600394+erwinsk@users.noreply.github.com>
2026-02-15 02:31:45 +0700

    Refactor logging and status handling in SerialThread
    disable enable tombol, pemisahan status

--- lutron_logger.py
+++ lutron_logger.py
@@ -94,11 +94,14 @@ def parse_frame(frame_bytes):
 class SerialThread(QThread):
     data_received = Signal(str, float, str, str)
     status_changed = Signal(str)
+    status_serial = Signal(str)
     connect_btn = Signal(bool)
+    start_btn = Signal(bool)
+    stop_btn = Signal(bool)
 
     def __init__(self):
         super().__init__()
-        self.debug = True
+        self.debug = False
         self.running = False
         self.logging_enabled = False
         self.port = None
@@ -145,11 +148,11 @@ class SerialThread(QThread):
             time.sleep(0.2)
             self.ser.reset_input_buffer()
 
-            self.status_changed.emit("Terhubung")
+            self.status_serial.emit("Terhubung")
             self.connect_btn.emit(False)
 
         except Exception:
-            self.status_changed.emit("Serial Error")
+            self.status_serial.emit("Serial Error")
             self.connect_btn.emit(True)
             return
 
@@ -192,22 +195,41 @@ class SerialThread(QThread):
                  # Logging dipicu saat pasangan lengkap + interval tercapai
                     now = time.time()
                     if (self.logging_enabled and self.writer and
-                        self.last_lower is not None and
                         now - self.last_log_time >= self.log_interval):
-                        if self.debug == True : print("Merekam data pada",time.time())
 
-                        self.writer.writerow([
-                            datetime.datetime.now().isoformat(),
-                            self.last_upper,
-                            self.last_middle,
-                            self.last_lower,
-                            self.unit_upper,
-                            self.unit_middle,
-                            self.unit_lower,
-                            self.polarity_upper,
-                            self.polarity_middle,
-                            self.polarity_lower
-                        ])
+                        if self.last_lower is not None:
+                            if self.debug == True : print("Merekam data 3 pada",time.time())
+                            self.writer.writerow([
+                                datetime.datetime.now().isoformat(),
+                                self.last_upper,
+                                self.last_middle,
+                                self.last_lower,
+                                self.unit_upper,
+                                self.unit_middle,
+                                self.unit_lower,
+                                self.polarity_upper,
+                                self.polarity_middle,
+                                self.polarity_lower
+                                 ])
+                        elif self.last_middle is not None and self.last_lower is None:
+                                if self.debug == True : print("Merekam data 2 pada",time.time())
+                                self.writer.writerow([
+                                datetime.datetime.now().isoformat(),
+                                self.last_upper,
+                                self.last_middle,
+                                self.unit_upper,
+                                self.unit_middle,
+                                self.polarity_upper,
+                                self.polarity_middle,
+                                ])
+                        elif self.last_lower is None and self.last_middle is None:
+                                if self.debug == True : print("Merekam data 1 pada",time.time())
+                                self.writer.writerow([
+                                datetime.datetime.now().isoformat(),
+                                self.last_upper,
+                                self.unit_upper,
+                                self.polarity_upper,
+                                ])
                         self.file_handle.flush()
                         self.last_log_time = now
 
@@ -222,20 +244,29 @@ class SerialThread(QThread):
         if self.ser and self.ser.is_open:
             self.ser.close()
 
-        self.status_changed.emit("Tidak Terhubung")
+        self.status_serial.emit("Tidak Terhubung")
 
     def start_logging(self):
         if not self.csv_file:
             self.status_changed.emit("CSV belum dipilih")
+            self.start_btn.emit(True)
+            self.stop_btn.emit(False)
             return
 
         try:
             self.file_handle = open(self.csv_file, "a", newline="")
             self.writer = csv.writer(self.file_handle)
-            self.writer.writerow(["timestamp","upper","middle","lower","unit_upper","unit_middle","unit_lower","polarity_upper","polarity_middle","polarity_lower"])
+            if self.last_lower is not None:
+                self.writer.writerow(["timestamp","upper","middle","lower","unit_upper","unit_middle","unit_lower","polarity_upper","polarity_middle","polarity_lower"])
+            elif self.last_middle is not None and self.last_lower is None:
+                self.writer.writerow(["timestamp","upper","middle","unit_upper","unit_middle","polarity_upper","polarity_middle",])
+            elif self.last_lower is None and self.last_middle is None:
+                self.writer.writerow(["timestamp","upper","unit_upper","polarity_upper",])
             self.logging_enabled = True
             self.last_log_time = 0
             self.status_changed.emit("Merekam")
+            self.start_btn.emit(False)
+            self.stop_btn.emit(True)
 
         except Exception:
             self.status_changed.emit("File Error")
@@ -246,7 +277,8 @@ class SerialThread(QThread):
             self.file_handle.close()
             self.file_handle = None
             self.writer = None
-        self.status_changed.emit("Terhubung - Tidak Merekam")
+        self.status_changed.emit("Tidak Merekam")
+        self.start_btn.emit(True)
 
     def stop(self):
         self.running = False
@@ -288,6 +320,7 @@ class MainWindow(QWidget):
 
         self.stop_btn = QPushButton("Stop Logging")
         self.stop_btn.clicked.connect(self.stop_logging)
+        self.stop_btn.setEnabled(False)
 
         self.exit_btn = QPushButton("Tutup Logger")
         self.exit_btn.clicked.connect(self.close)
@@ -301,7 +334,8 @@ class MainWindow(QWidget):
         self.lower_label = QLabel("--")
         self.lower_label.setStyleSheet("font-size: 28px;")
 
-        self.status_label = QLabel("Status: Terputus")
+        self.status_label = QLabel("File Log: Tidak Merekam")
+        self.status_serial = QLabel("Koneksi: Terputus")
 
         layout = QVBoxLayout()
 
@@ -322,6 +356,7 @@ class MainWindow(QWidget):
         layout.addWidget(self.file_btn)
         layout.addWidget(self.start_btn)
         layout.addWidget(self.stop_btn)
+        layout.addWidget(self.status_serial)
         layout.addWidget(self.status_label)
 
         layout.addWidget(QLabel("Display 1 (Header 41)"))
@@ -339,7 +374,10 @@ class MainWindow(QWidget):
 
         self.serial_thread.data_received.connect(self.update_display)
         self.serial_thread.status_changed.connect(self.update_status)
+        self.serial_thread.status_serial.connect(self.update_status_serial)
         self.serial_thread.connect_btn.connect(self.connect_serial)
+        self.serial_thread.start_btn.connect(self.start_btn.setEnabled)
+        self.serial_thread.stop_btn.connect(self.stop_btn.setEnabled)
 
     def refresh_ports(self):
         self.port_combo.clear()
@@ -369,11 +407,11 @@ class MainWindow(QWidget):
         interval = float(self.interval_combo.currentText())
         self.serial_thread.set_interval(interval)
         self.serial_thread.start_logging()
-        self.start_btn.setEnabled(False)
+        #self.start_btn.setEnabled(False)
 
     def stop_logging(self):
         self.serial_thread.stop_logging()
-        self.start_btn.setEnabled(True)
+        #self.start_btn.setEnabled(True)
 
     def update_display(self, display, value, unit, polarity):
         text = f"{polarity}{value:.2f} {unit}"
@@ -385,7 +423,10 @@ class MainWindow(QWidget):
             self.middle_label.setText(text)
 
     def update_status(self, status):
-        self.status_label.setText(f"Status: {status}")
+        self.status_label.setText(f"File Log: {status}")
+        
+    def update_status_serial(self, status):
+        self.status_serial.setText(f"Koneksi: {status}")
 
     def closeEvent(self, event):
         self.serial_thread.stop()
